d2de374915b2120a00f3bae97299b76a9ddc0b5f
Rockky <devil_rules_universe@yahoo.co.in>
2012-10-10 19:19:53 +0530

    coomit  10/10/2012

---
+++ CareForU/Controllers/GridViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CareLib.Repository;
+
+namespace CareForU.Controllers
+{
+    public class GridViewController : Controller
+    {
+
+        ICareDoctor _careDB;
+        public GridViewController()
+        {
+            _careDB = new DoctorDal(new CareLib.Models.CareForYouEntities());
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Tittle = "Meet Our Doctors";
+            var test = _careDB.getAllDoctors();
+            ViewData.Model = test;
+            return View();
+        }
+
+    }
+}
---
+++ CareForU/Scripts/MyScript.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="jquery-1.8.2-vsdoc.js" />
+/// <reference path="jquery-ui-1.8.11.js" />
+
+
+$(document).ready(function () {
+
+    $('#gvTheGrid tbody tr').live('hover', function () {
+        $(this).toggleClass('clickable');
+    }).live('click', function () {
+        location.href = '/Details/' + $(this).find('td:last').text();
+    });
+  
+    
+});
